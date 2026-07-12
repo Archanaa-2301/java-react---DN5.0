b99a7c3bccce0cdac2d498341128188391f4c058
Archanaa-2301 <archanaa8642@gmail.com>
2026-07-12 23:07:00 +0530

    Week 3 - Exercise 1: Basic Spring Application

---
+++ Week 3/SpringCore_Maven/LibraryManagement/src/main/java/com/library/main/LibraryManagementApplication.java
@@ -0,0 +1,20 @@
+package com.library.main;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.library.service.BookService;
+
+public class LibraryManagementApplication {
+
+    public static void main(String[] args) {
+
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        BookService service = context.getBean("bookService", BookService.class);
+
+        service.displayService();
+
+        context.close();
+    }
+}
