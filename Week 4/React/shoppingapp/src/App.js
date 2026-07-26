017052b743d2a36df5cacd81d3d02ed2dc9234c5
Archanaa-2301 <archanaa8642@gmail.com>
2026-07-26 18:02:31 +0530

    Completed React Exercise 2 - Shopping App

---
+++ Week 4/React/shoppingapp/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import OnlineShopping from './components/OnlineShopping';
+
+function App() {
+  return (
+    <div className="App">
+      <OnlineShopping />
+    </div>
+  );
+}
+
+export default App;
